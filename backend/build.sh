f3a92b08e5a0c17ce40d452605fd16055fefc1a9
Gabriel Corrêa <gabrielcorrreaa@gmail.com>
2025-11-18 13:40:58 -0300

    Colocado build

---
+++ backend/build.sh
@@ -0,0 +1,7 @@
+
+
+# Instala todas as dependências do Python
+pip install -r requirements.txt
+
+# Executa um comando Python para baixar os pacotes NLTK
+python -c "import nltk; nltk.download('all')"
