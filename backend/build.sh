fe8074f70127661097e96465a1440d4b91eb1bbf
Gabriel Corrêa <gabrielcorrreaa@gmail.com>
2025-11-18 13:53:27 -0300

    Retirado o download nltk

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,3 @@
 
 # Instala todas as dependências do Python
 pip install -r requirements.txt
-
-# Executa um comando Python para baixar os pacotes NLTK
-
-python -c "import nltk; nltk.download('all')"
